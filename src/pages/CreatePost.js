338ab9fa2be1d1d96332544b7eab82a40c5ccedc
Jeongin Yoon <jy3524@nyu.edu>
2021-12-06 21:24:33 -0500

    styling with mui

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { initializeApp } from '@firebase/app';
 import { getFirestore, collection, addDoc } from '@firebase/firestore';
 import firebaseConfig from '../components/FirebaseConfig';
+import { Grid, Paper, Button, TextField }from '@mui/material';
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
@@ -23,19 +24,26 @@ function CreatePost() {
     setAuthor("")
   }
 
+  const paperStyle={padding:20, height:'60vh', width: '70%', margin:"30px auto"};
+
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>Post</h1>
-      <div>
-        <label>Title</label>
-        <input placeholder="Type here" value={title} onChange={(e) => setTitle(e.target.value)} />
-        <label>Text</label>
-        <textarea placeholder="Type here" value={text} onChange={(e) => setText(e.target.value)}/>
-        <lable>Name</lable>
-        <input placeholder="Name" value={author} onChange={(e) => setAuthor(e.target.value)}/>
-      </div>
-      <button onClick={handleSubmit}>Submit Post</button>
-    </form>
+    <Grid align="center">
+      <Paper elevation={10} style={paperStyle}>
+        <form onSubmit={handleSubmit}>
+          <h1>Post</h1>
+          <div>
+            <TextField sx={{ m: 1 }} style={{width: "80%"}} id="outlined-name" label="Title" placeholder="Type here" value={title} onChange={(e) => setTitle(e.target.value)} />
+          </div>
+          <div>
+            <TextField sx={{ m: 1 }} style={{width: "80%"}} id="outlined-name" label="Text" placeholder="Type here" value={text} onChange={(e) => setText(e.target.value)}/>
+          </div>
+          <div>
+            <TextField sx={{ m: 1 }} style={{width: "80%"}} id="outlined-name" label="Name" placeholder="Name" value={author} onChange={(e) => setAuthor(e.target.value)}/>
+          </div>
+          <Button sx={{ m: 3 }} style={{width: "80%"}} type="submit" color="primary" variant="contained" onClick={handleSubmit}>Submit Post</Button>
+        </form>
+      </Paper>
+    </Grid>
   )
 }
 
