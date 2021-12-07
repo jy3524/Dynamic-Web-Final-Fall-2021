2a384a4ac4ca5051cba97325997a0e54a77db8b2
Jeongin Yoon <jy3524@nyu.edu>
2021-12-07 13:56:43 -0500

    add user profile

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Paper } from "@mui/material";
+import { Avatar, Grid, Paper } from "@mui/material";
 
 function UserProfile( { userInformation }) {
   const paperStyle={padding:20, height:'50vh', width:300, margin:"30px auto"};
@@ -9,6 +9,7 @@ function UserProfile( { userInformation }) {
       <Paper elevation={10} style={paperStyle}>
         <Grid align="center">
           <h1>User Profile</h1>
+          <Avatar sx={{height:"70px", width:"70px"}} alt="" src="/broken-img.jpg"/>
           <p>EMAIL: {userInformation.email}</p>
           <p>NAME: {userInformation.displayName}</p>
           <p>UID: {userInformation.uid}</p>
