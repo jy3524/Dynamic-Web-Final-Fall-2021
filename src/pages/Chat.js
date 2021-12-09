3fb707337b513e9b1f60e9b7bace8213573abb78
Jeongin Yoon <jy3524@nyu.edu>
2021-12-09 15:49:45 -0500

    working on chat function

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,12 +1,55 @@
-import React from 'react';
-import { Grid }from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { initializeApp } from '@firebase/app';
+import { getFirestore, addDoc, getDocs, collection } from '@firebase/firestore';
+import firebaseConfig from '../components/FirebaseConfig';
+import { Avatar, Button, Grid, TextField }from '@mui/material';
+import { red } from '@mui/material/colors';
+import SendIcon from '@mui/icons-material/Send';
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 function Chat() {
-  return (
-    <Grid align="center">
-      <h1>Work in Progress</h1>
-    </Grid>
-  )
-}
+  const [text, setText] = useState("");
+  const [chats, setChat] = useState([]);
 
-export default Chat
+  const handleSubmit = async(e) => {
+    e.preventDefault(); 
+    await addDoc(collection(db, "chat"), {
+      text,
+    });
+    setText("")
+    window.location.reload();
+  }
+
+  useEffect(() => {
+    const getChat = async () => {
+      const data = await getDocs(collection(db, "chat"));
+      setChat(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getChat();
+  }, []);
+
+  return (
+    <Grid onSubmit={handleSubmit} align="center">
+      <h1>Let's chat</h1>
+      <div>
+        {chats.map((chat) => {
+          return (
+            <div>
+              <Avatar sx={{height:"50px", width:"50px", bgcolor:red[500]}} 
+              alt="" src="/broken-img.jpg" />
+              <p>{chat.text}</p>
+            </div>
+          )
+        })}
+          <TextField label="Type chat here" variant="standard" value={text} onChange={(e) => setText(e.target.value)}/>
+          <Button variant="contained" endIcon={<SendIcon />} onClick={handleSubmit}>
+            Send
+          </Button>
+      </div>
+    </Grid>
+    )
+}  
+
+export default Chat;
