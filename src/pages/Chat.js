4bde0a9e50dce59a46732a0861d08dfd9f6439df
Jeongin Yoon <jy3524@nyu.edu>
2021-12-10 15:21:13 -0500

    working on chat function

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { initializeApp } from '@firebase/app';
-import { getFirestore, addDoc, getDocs, collection } from '@firebase/firestore';
+import { getFirestore, addDoc, collection, serverTimestamp, query, orderBy, onSnapshot } from '@firebase/firestore';
 import firebaseConfig from '../components/FirebaseConfig';
-import { Avatar, Button, Grid, TextField }from '@mui/material';
+import { Avatar, Button, Grid, List, Paper, TextField }from '@mui/material';
 import { red } from '@mui/material/colors';
 import SendIcon from '@mui/icons-material/Send';
+import { Box } from '@mui/system';
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
@@ -17,37 +18,49 @@ function Chat() {
     e.preventDefault(); 
     await addDoc(collection(db, "chat"), {
       text,
+      createdAt: serverTimestamp(),
     });
     setText("")
     window.location.reload();
   }
 
   useEffect(() => {
-    const getChat = async () => {
+    const q = query(collection(db, "chat"), orderBy("createdAt", "asc"));
+    const getChat = onSnapshot(q, (snapshot) => 
+      setChat(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+    );
+    return getChat
+
+    /*const getChat = async () => {
       const data = await getDocs(collection(db, "chat"));
       setChat(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
-    getChat();
+    getChat();*/
+
   }, []);
 
   return (
     <Grid onSubmit={handleSubmit} align="center">
       <h1>Let's chat</h1>
-      <div>
-        {chats.map((chat) => {
-          return (
-            <div>
-              <Avatar sx={{height:"50px", width:"50px", bgcolor:red[500]}} 
-              alt="" src="/broken-img.jpg" />
-              <p>{chat.text}</p>
-            </div>
-          )
-        })}
-          <TextField label="Type chat here" variant="standard" value={text} onChange={(e) => setText(e.target.value)}/>
-          <Button variant="contained" endIcon={<SendIcon />} onClick={handleSubmit}>
-            Send
-          </Button>
-      </div>
+      <Box sx={{ pb:7 }}>
+        <List>
+          {chats.map((chat) => {
+            return (
+              <Paper sx={{margin:"30px", padding:"10px", borderRadius:"30px"}} elevation={10}>
+                <Avatar sx={{height:"30px", width:"30px", bgcolor:red[500]}} 
+                alt="" src="/broken-img.jpg" />
+                <p>{chat.text}</p>
+              </Paper>
+            )
+          })}
+        </List>
+      </Box>
+      <Paper sx={{ position:'fixed', bottom: 0, left: 0, right: 0}} elevation={10}>
+        <TextField label="Type chat here" variant="standard" value={text} onChange={(e) => setText(e.target.value)}/>
+        <Button sx={{ margin: '10px'}} variant="contained" endIcon={<SendIcon />} onClick={handleSubmit}>
+          Send
+        </Button>
+      </Paper>
     </Grid>
     )
 }  
