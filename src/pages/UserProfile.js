7675ed30694daf72a10f12a9b3ce1968096c2e61
Jeongin Yoon <jy3524@nyu.edu>
2021-12-10 16:25:23 -0500

    styling using mui

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Avatar, Grid, Paper } from "@mui/material";
+import { red } from '@mui/material/colors';
 
 function UserProfile( { userInformation }) {
   const paperStyle={padding:20, height:'50vh', width:300, margin:"30px auto"};
@@ -8,11 +9,12 @@ function UserProfile( { userInformation }) {
     <Grid>
       <Paper elevation={10} style={paperStyle}>
         <Grid align="center">
-          <h1>User Profile</h1>
-          <Avatar sx={{height:"70px", width:"70px"}} alt="" src="/broken-img.jpg"/>
-          <p>EMAIL: {userInformation.email}</p>
-          <p>NAME: {userInformation.displayName}</p>
-          <p>UID: {userInformation.uid}</p>
+          <h1>Profile</h1>
+          <Avatar sx={{height:"70px", width:"70px", bgcolor:red[500]}} alt="" src="/broken-img.jpg"/>
+          <p className="userprofile">{userInformation.displayName}</p>
+          <p>Lorem ipsum dolor sit amet</p>
+          <p className="userprofile">{userInformation.email}</p>
+          <p className="userprofile">ID: {userInformation.uid}</p>
         </Grid>
       </Paper>
     </Grid>
